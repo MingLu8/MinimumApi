1d51e850cd026f75eb6915c470032bc2884a1ea4
ming lu <ming.lu@nuvoair.com>
2023-12-03 09:52:43 -0500

    added validation unit test.

---
+++ src/MinimalApi.UnitTests/MinimalApi.UnitTests/PersonServiceTests.cs
@@ -0,0 +1,39 @@
+using FluentAssertions;
+using FluentValidation;
+using FluentValidation.Results;
+using MinimumApi.Entities;
+using MinimumApi.Exceptions;
+using MinimumApi.Repositories;
+using MinimumApi.Services;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+
+namespace MinimalApi.UnitTests
+{
+    public class PersonServiceTests
+    {
+        [Fact]
+        public void AddPersonAsync_auto_populates_CreateDateUTC()
+        {
+            var validator = Substitute.For<IValidator<Person>>();
+            var repo = Substitute.For<IPersonRepository>();
+            var service = new PersonService(validator, repo);
+            var person = new Person { Age = 1, Name = "x" };
+            service.AddPersonAsync(person);
+            repo.Received(1).AddPersonAsync(Arg.Is<Person>(p=> p.CreatedDateUtc.HasValue));
+        }
+
+        [Fact]
+        public void UpdatePersonAsync_throws_validation_exception_when_person_does_not_exist()
+        {
+            var validationResult = new ValidationResult(new List<ValidationFailure> { new ValidationFailure("Age", "error") });
+            var validator = Substitute.For<IValidator<Person>>();
+            validator.ValidateAsync(Arg.Any<Person>()).Returns(validationResult);
+
+            var repo = Substitute.For<IPersonRepository>();
+            var service = new PersonService(validator, repo);
+            service.UpdatePersonAsync(new Person())
+                .Should().Throws<ValidationFailedException>();
+        }
+    }
+}
